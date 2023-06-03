b5edcfb2b082656cca21a0aedaaa47148a08c955
christiano <robnelito.christiano.nga@gmail.com>
2023-06-03 18:18:21 +0300

    ft:dotenv-secure the ap

---
+++ README.md
@@ -0,0 +1,14 @@
+# API Gestion Bancaire
+
+## Guide d'installation & configuration
+
+1. Installation des dépendances
+
+````console
+npm install
+````
+
+2. Configuration de l'envirennement
+
+
+
--- app/models/pollpg.js
+++ app/models/pollpg.js
@@ -1,11 +1,13 @@
+const {prepareValue} = require("pg/lib/utils");
+require('dotenv').config()
 const Poll = require('pg').Pool
 
 const pool = new Poll({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'javaav',
-    password: 'root',
-    port: 5432,
+    user: process.env.PG_USER_NAME,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DB,
+    password: process.env.PG_USER_PASSWORD,
+    port: process.env.PG_PORT,
 })
 
 module.exports = {
