76749d07d508d072c679cce0deb618831224146f
christiano <robnelito.christiano.nga@gmail.com>
2023-06-06 18:29:13 +0300

    refactor(routes):remove auth midd from the client routes

--- app/routes/client.js
+++ app/routes/client.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const clientController = require('./../controllers/client')
 const auth = require("./../middleware/auth");
 
-router.get('/', auth, clientController.getClients)
+router.get('/', clientController.getClients)
 router.get('/:numero_compte', clientController.getClient)
 router.post('/', clientController.createClient)
 router.put('/:numero_compte', clientController.updateClient)
