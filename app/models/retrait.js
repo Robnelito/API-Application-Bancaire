ec33183264bdba6998b2bddea6c0e2e46e261b9b
HarryToky <harrytoky123@gmail.com>
2023-06-07 20:23:25 +0300

    add: all

--- app/models/retrait.js
+++ app/models/retrait.js
@@ -13,7 +13,8 @@ const getClientSolde = (req, res) => {
 
 const retrait = (req, res) => {
     const numero_compte = parseInt(req.params.numero_compte);
-    const montant = parseFloat(req.body.montant);
+    const montant_retrait = parseFloat(req.body.montant);
+    const numero_cheque = req.body.numero_cheque;
 
     // Récupérer le solde actuel du client
     pool.query('SELECT solde FROM client WHERE numero_compte = $1', [numero_compte], (error, results) => {
@@ -23,10 +24,10 @@ const retrait = (req, res) => {
 
         const solde = parseFloat(results.rows[0].solde);
 
-        if (solde < montant) {
+        if (solde < montant_retrait) {
             res.status(400).send('Solde insuffisant pour effectuer le retrait.'); // Renvoyer un message d'erreur si le solde est insuffisant
         } else {
-            const nouveauSolde = solde - montant;
+            const nouveauSolde = solde - montant_retrait;
 
             // Mettre à jour le solde du client après le retrait
             pool.query('UPDATE client SET solde = $1 WHERE numero_compte = $2', [nouveauSolde, numero_compte], (error) => {
@@ -35,15 +36,17 @@ const retrait = (req, res) => {
                 }
 
                 // Insérer les données de retrait dans la table "retrait"
-                const dateRetrait = new Date(); // Obtenir la date et l'heure actuelles
-                const values = [numero_compte, montant, dateRetrait];
-                console.log(dateRetrait);
-                pool.query('INSERT INTO retrait (numero_compte, montant, date_retrait) VALUES ($1, $2, $3)', values, (error) => {
+                const dateRetrait = new Date(); // Obtenez votre date de retrait
+                const options = { day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+                const dateFormatee = dateRetrait.toLocaleString(undefined, options); // Obtenir la date et l'heure actuelles
+                
+                const values = [numero_compte, numero_cheque, montant_retrait, dateFormatee];
+                console.log(dateFormatee);
+                pool.query('INSERT INTO retrait (numero_compte, numero_cheque, montant_retrait, date_retrait) VALUES ($1, $2, $3, $4)', values, (error) => {
                     if (error) {
                         throw error;
                     }
-
-                    res.status(200).send(`Retrait de ${montant} effectué avec succès. Nouveau solde : ${nouveauSolde}`);
+                    res.status(200).send(`Retrait de ${montant_retrait} effectué avec succès. Nouveau solde : ${nouveauSolde}`);
                 });
             });
         }
